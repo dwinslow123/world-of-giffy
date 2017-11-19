7c2e7310dfb947ef9d370595ec471790677e987a
Dan Winslow <dwinslow123@me.com>
2017-11-19 14:06:04 -0600

    working on deployment

--- js/giphy.js
+++ js/giphy.js
@@ -1,35 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-	
-	req = new XMLHttpRequest;
-	req.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC', true);
-	
-	req.onload = function() {
-		if (req.status >= 200 && req.status < 400){
-      console.log(req.responseText);
-			const data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
-			console.log(data);
-			document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">';
-		} else {
-      console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
-    }
-	};
-  
-  req.send();
-  
-  document.getElementById('btn').addEventListener('click', () => {
-    req = new XMLHttpRequest;
+	const createCORSRequest = (method, url) => {
+    req = new XMLHttpRequest();
     req.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC', true);
     
-    req.onload = () => {
+    req.onload = function() {
       if (req.status >= 200 && req.status < 400){
         console.log(req.responseText);
-        data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
+        const data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
         console.log(data);
         document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">';
       } else {
         console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
-       }
+      }
     };
+    
     req.send();
-  });
+    
+    document.getElementById('btn').addEventListener('click', () => {
+      req = new XMLHttpRequest;
+      req.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC', true);
+      
+      req.onload = () => {
+        if (req.status >= 200 && req.status < 400){
+          console.log(req.responseText);
+          data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
+          console.log(data);
+          document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">';
+        } else {
+          console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
+        }
+      };
+      req.send();
+    });
+  }
 });
