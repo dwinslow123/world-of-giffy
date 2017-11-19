b5f06ffd4f0c41277d9e287d7ee4800df05f4dc5
Dan Winslow <dwinslow123@me.com>
2017-11-19 13:53:57 -0600

    added a button that refreshes the GIF

--- js/giphy.js
+++ js/giphy.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
 	
 	req = new XMLHttpRequest;
 	req.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC', true);
@@ -6,13 +6,30 @@ document.addEventListener('DOMContentLoaded', function () {
 	req.onload = function() {
 		if (req.status >= 200 && req.status < 400){
       console.log(req.responseText);
-			data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
+			const data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
 			console.log(data);
 			document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">';
 		} else {
-			console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
-		 }
+      console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
+    }
 	};
-
+  
   req.send();
+  
+  document.getElementById('btn').addEventListener('click', () => {
+    req = new XMLHttpRequest;
+    req.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC', true);
+    
+    req.onload = () => {
+      if (req.status >= 200 && req.status < 400){
+        console.log(req.responseText);
+        data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
+        console.log(data);
+        document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">';
+      } else {
+        console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
+       }
+    };
+    req.send();
+  });
 });
