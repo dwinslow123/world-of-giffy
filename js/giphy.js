59ac3801ccf84f65ef10cb868569ee78a743f28c
Dan Winslow <dwinslow123@me.com>
2017-11-21 08:06:50 -0600

    realized this could be done a lot simpler with jquery

--- js/giphy.js
+++ js/giphy.js
@@ -1,54 +1,23 @@
-// this is to help prevent Cross browser issues
-const createCORSRequest = (method, url) => {
-  req = new XMLHttpRequest();
-  if ("withCredentials" in req) {
-    req.open(method, url, true);
-  } else if (typeof XDomainRequest !== "undefined") {
-    req = new XDomainRequest();
-    req.open(method, url);
-  } else {
-    req = null;
-  }
-  return req;
-}
+$(document).ready(() => {
+  const $search = $('.search');
+  const $formValue = $('#form-value');
+  const $gif = $('#gif');
+  const $reset = $('#reset');
+  const apiKey = '&api_key=dc6zaTOxFJmzC'
+  const url = 'http://api.giphy.com/v1/gifs/search?q='
 
-document.addEventListener('DOMContentLoaded', () => { // waits for content to load before executing
-  const url = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
-
-  const req = createCORSRequest('GET', url); // makes the request to the Giphy API
-  if (!req) alert('CORS not supported');
-
-	req.onload = () => {
-		if (req.status >= 200 && req.status < 400){
-      console.log(req.responseText);
-			const data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
-			console.log(data);
-			document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">'; // this places the gif in the html element
-		} else {
-      console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
-    }
-	};
+        $search.on('click', () => {
+        let userInput = $formValue.val().trim();  
+          userInput = userInput.replace(/ /g, "+");
+          const queryURL = url + userInput + apiKey;
   
-  req.send(); // sends the gif
-  
-
-  // this does the same thing as before, but only when the button is clicked
-  document.getElementById('btn').addEventListener('click', () => {
-  const url = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
-
-  const req = createCORSRequest('GET', url);
-  if (!req) alert('CORS not supported');
-  
-    req.onload = () => {
-      if (req.status >= 200 && req.status < 400){
-        console.log(req.responseText);
-        data = JSON.parse(req.responseText).data.fixed_height_downsampled_url;
-        console.log(data);
-        document.getElementById("output").innerHTML = '<img src = "'+data+'"  title="GIF from Giphy">';
-      } else {
-        console.log('SOMETHING WENT WRONG! You reached Giphy, but the API returned an error!');
-       }
-    };
-    req.send();
-  })
-});
+          $.ajax({url: queryURL, method: 'GET'}).done( (response) => {
+              const giphyURL = response.data[Math.floor(Math.random()) * 25].images.fixed_height.url;              
+            $gif.attr('src', giphyURL);
+          });
+         $reset.on('click', () => {
+            $gif.attr("src",'');
+          });
+          return false;
+        })
+      });
